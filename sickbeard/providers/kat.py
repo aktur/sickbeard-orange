152a8dcb3ed99a08ebdab1449c76e20823947578
Michal Rutka <michal.rutka@gmail.com>
2020-07-25 17:59:13 +0200

    Change KAT url

--- sickbeard/providers/kat.py
+++ sickbeard/providers/kat.py
@@ -54,7 +54,7 @@ class KATProvider(generic.TorrentProvider):
 
         self.cache = KATCache(self)
         
-        self.url = 'http://kickass.to/'
+        self.url = 'https://kickasstorrents.cr/'
 
         self.searchurl = self.url+'usearch/%s/?field=seeders&sorder=desc'  #order by seed       
 
